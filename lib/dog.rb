510a8adddd2cc2cac5a11232eec7fc7a722c9eea
Brian Matheny <bwmatheny@gmail.com>
2020-05-19 15:43:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Add your code here
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@all.each {|name| puts "#{name}"}
+  end
+
+  
+
+end
